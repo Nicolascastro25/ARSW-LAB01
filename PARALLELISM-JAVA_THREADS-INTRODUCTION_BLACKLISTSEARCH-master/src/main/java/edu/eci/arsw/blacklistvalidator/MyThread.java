0568fa153f475f17cb8282af14ee16de42ff6756
Jaime Nicolás Castro Acuña <jaime.nicolas.castro@gmail.com>
2022-08-10 09:49:25 -0500

    Add files via upload

---
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH-master/src/main/java/edu/eci/arsw/blacklistvalidator/MyThread.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.eci.arsw.blacklistvalidator;
+
+/**
+ *
+ * @author jaime.cacuna
+ */
+public class MyThread extends Thread{
+    
+    public void run(){
+    
+    }
+}
---
+++ PARALLELISM-JAVA_THREADS-INTRODUCTION_BLACKLISTSEARCH-master/src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThread extends Thread {
+    
+    int a = 0;
+    int b = 0;
+    String nombreHilo = null;
+    
+    @Override
+    public void run() {        
+        for (int i = a; i < b; i++) {
+            System.out.println(i+" "+nombreHilo);
+        }
+    }
+
+    public String getNombreHilo() {
+        return nombreHilo;
+    }
+
+    public void setNombreHilo(String nombreHilo) {
+        this.nombreHilo = nombreHilo;
+    }
+    
+    
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
+}
